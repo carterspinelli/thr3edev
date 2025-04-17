04269b421bcd33ce9bc3aebf687b84c758af14dc
carter <129820642+carterspinelli@users.noreply.github.com>
2025-04-17 00:36:39 +0000

    Restored to 'cabc1bdad2a15da04441fd697bfede9fde3ca4f4'
    Replit-Restored-To: cabc1bdad2a15da04441fd697bfede9fde3ca4f4

--- client/src/components/ui/service-custom-icons.tsx
+++ client/src/components/ui/service-custom-icons.tsx
@@ -2,17 +2,29 @@
 import React from "react";
 
 export const ChessKnightIcon = () => (
-  <img src="/horse.png" alt="Strategy" style={{ width: "80px", height: "80px" }} />
+  <svg width="80" height="80" viewBox="0 0 80 80" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path d="M50 10C50 10 47 14 43 15C39 16 36 20 36 20C36 20 32 20 30 27C28 34 23 35 23 35L21 42L28 46L26 53L38 63C38 63 38 68 37 70H56C56 70 56 65 54 63L62 55C64 53 65 51 65 47C65 43 60 40 60 40L64 30C64 30 65 25 60 20C55 15 50 10 50 10Z" />
+    <path d="M25 70H55C55 70 55 65 53 63H27C27 63 27 67 25 70Z" />
+  </svg>
 );
 
 export const PencilIcon = () => (
-  <img src="/cube.png" alt="Design" style={{ width: "80px", height: "80px" }} />
+  <svg width="80" height="80" viewBox="0 0 80 80" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path d="M60 15L65 20L25 60H20V55L60 15Z" />
+    <path d="M15 65H65V70H15V65Z" />
+  </svg>
 );
 
 export const LaptopIcon = () => (
-  <img src="/laptop.png" alt="Development" style={{ width: "80px", height: "80px" }} />
+  <svg width="80" height="80" viewBox="0 0 80 80" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path d="M15 20C15 17.2386 17.2386 15 20 15H60C62.7614 15 65 17.2386 65 20V50H15V20Z" />
+    <path d="M10 55C10 52.2386 12.2386 50 15 50H65C67.7614 50 70 52.2386 70 55V60C70 62.7614 67.7614 65 65 65H15C12.2386 65 10 62.7614 10 60V55Z" />
+  </svg>
 );
 
 export const SpeedometerIcon = () => (
-  <img src="/speed.png" alt="Speed" style={{ width: "80px", height: "80px" }} />
+  <svg width="80" height="80" viewBox="0 0 80 80" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path d="M40 15C26.2 15 15 26.2 15 40C15 53.8 26.2 65 40 65C53.8 65 65 53.8 65 40C65 26.2 53.8 15 40 15ZM40 60C29 60 20 51 20 40C20 29 29 20 40 20C51 20 60 29 60 40C60 51 51 60 40 60Z" />
+    <path d="M53 27L39 42C39 42 36 41 35 42C34 43 33 46 35 48C37 50 40 49 41 48C42 47 41 44 41 44L55 30L53 27Z" />
+  </svg>
 );
